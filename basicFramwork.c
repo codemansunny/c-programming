49285944a8ae297f7dc2b3e239435cda5398ecfe
codemansunny <ahsanhabibsunny@hotmail.com>
2022-01-02 16:19:09 +0600

    Basic Framwork

---
+++ basicFramwork.c
@@ -0,0 +1,8 @@
+// If I Need To Someting Return A Value I Use This Function To Return It To The Caller Int 
+int main (){
+    return 0;
+}
+// If I Need TO Print Nathing I Use This Function Caller Void
+void main (){
+    return;
+}
